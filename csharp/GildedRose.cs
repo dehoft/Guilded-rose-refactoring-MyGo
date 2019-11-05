d1435a69068a2a393a0eb7c427fb6c403c0a0ffe
dehoft <edvinas.deksnys@gmail.com>
2019-11-05 15:12:57 +0200

    2 fixed AgedBrieUpdate method logics and added BackStagePasses method

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,10 +14,18 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                
-                if (Items[i].Name == "Aged Brie")
+
+
+                string[] name = Items[i].Name.Split(' ');
+                string firstTwoNameWords = name[0] + " " + name[1];
+
+                if (firstTwoNameWords == "Aged Brie")
                 {
                     AgedBrieUpdate(i);
+                }
+                else if(firstTwoNameWords == "Backstage passes")
+                {
+                    BackstagePassesUpdate(i);
                 }
 
 
@@ -39,6 +47,45 @@ namespace csharp
                         Items[i].Quality = Items[i].Quality + 2;
                         break;
                 }
+
+                if(Items[i].Quality > 50)
+                {
+                    Items[i].Quality = 50;
+                }
+            }
+
+            Items[i].SellIn--;
+        }
+
+        private void BackstagePassesUpdate(int i)
+        {
+            if (Items[i].SellIn > 0)
+            {
+                if (Items[i].Quality < 50)
+                {
+                    if (Items[i].SellIn <= 10 && Items[i].SellIn > 5)
+                    {
+                        Items[i].Quality = Items[i].Quality + 2;
+                    }
+                    else if (Items[i].SellIn <= 5 && Items[i].SellIn >= 0)
+                    {
+                        Items[i].Quality = Items[i].Quality + 3;
+                    }
+                    else
+                    {
+                        Items[i].Quality++;
+                    }
+
+                    if (Items[i].Quality > 50)
+                    {
+                        Items[i].Quality = 50;
+                    }
+                }
+                
+            }
+            else
+            {
+                Items[i].Quality = Items[i].Quality - Items[i].Quality;
             }
 
             Items[i].SellIn--;
